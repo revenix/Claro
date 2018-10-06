3a7268633ab4584a7cb4899c5b7ed4283927f190
jordy moises sullca quispe <moises.97.24@hotmail.com>
2018-10-05 19:39:51 -0500

    fecha

--- UstClaroSolution/LibreriaWorkflowActivity/Ust_CreateExcelTrasu.cs
+++ UstClaroSolution/LibreriaWorkflowActivity/Ust_CreateExcelTrasu.cs
@@ -14,78 +14,175 @@ using Libreria_ExportExcel.DTO;
 
 namespace UstClaro_WorkFlows
 {
-    public sealed class Ust_CreateExcelTrasu : CodeActivity
+  public sealed class Ust_CreateExcelTrasu : CodeActivity
+  {
+
+    protected override void Execute(CodeActivityContext executionContext)
     {
+      //Create the tracing service
+      ITracingService tracingService = executionContext.GetExtension<ITracingService>();
 
-        protected override void Execute(CodeActivityContext executionContext)
+      //Create the context
+      IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+      IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+      IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+      UrlFile.Set(executionContext, "");
+
+      GenerarExcel export = new GenerarExcel();
+
+      try
+      {
+        // var columna = Util.GetCrmConfiguration(service , "3108" );
+
+        string fetch2 = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                          <entity name='incident'>
+                            <attribute name='title' />
+                            <attribute name='ust_osiptelcomplaintid' />
+                            <attribute name='ust_osiptelcasegenerated' />
+                            <attribute name='ust_customername' />
+                            <attribute name='ust_trasurecordnumber' />
+                            <attribute name='ust_trasuemissiondate' />
+                            <attribute name='statuscode' />
+                            <attribute name='incidentid' />
+                            <attribute name='amxperu_casetype' />
+                            <attribute name='ust_serviceissueenddate' />
+                            <attribute name='ust_serviceissuestartdate' />
+                            <order attribute='title' descending='false' />
+                          </entity>
+                        </fetch>";
+
+
+        EntityCollection lista = service.RetrieveMultiple(new FetchExpression(fetch2));
+
+        var columna_EMAILEO = GetCrmConfiguration(service, "[GeneracionExcelTRASU-Columna]-EMAILEO");
+        var columna_EMAILNOTEO = GetCrmConfiguration(service, "[GeneracionExcelTRASU-Columna]-EMAILNOTEO");
+        var columna_NOMEO = GetCrmConfiguration(service, "[GeneracionExcelTRASU-Columna]-NOMEO");
+        var columna_NUMCARTALTRASU = GetCrmConfiguration(service, "[GeneracionExcelTRASU-Columna]-NUMCARTALTRASU");
+        var columna_NUMRUCEO = GetCrmConfiguration(service, "[GeneracionExcelTRASU-Columna]-NUMRUCEO");
+
+        var FechaActual = DateTime.Now.ToString("dd.mm.yyyy");
+
+
+        if (lista != null)
         {
-            //Create the tracing service
-            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
 
-            //Create the context
-            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
-            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
-            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+          List<Trasu> listaExcel = new List<Trasu>();
+          foreach (Entity item in lista.Entities)
+          {
 
-            UrlFile.Set(executionContext, "" );
 
-            GenerarExcel export = new GenerarExcel();
+            EntityReference entref = (EntityReference)item["amxperu_casetype"];
+            var LookupCaseType = entref.Name;
+            var TipoCaso = "";
 
-            try
+            if (LookupCaseType != null)
             {
-                // var columna = Util.GetCrmConfiguration(service , "3108" );
-
-                string fetch2 = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
-                                  <entity name='incident'>
-                                    <attribute name='title' />
-                                    <attribute name='ticketnumber' />
-                                    <attribute name='createdon' />
-                                    <attribute name='caseorigincode' />
-                                    <attribute name='etel_isdispute' />
-                                    <attribute name='incidentid' />
-                                    <order attribute='title' descending='false' />
-                                  </entity>
-                                </fetch>";
-
-
-                   EntityCollection lista = service.RetrieveMultiple(new FetchExpression(fetch2));
-
-                if (lista != null )
-                {
-
-                    List<Trasu> listaExcel = new List<Trasu>();
-                    foreach (Entity item in lista.Entities)
-                    {
-
-                        Trasu obj = new Trasu();
-                        obj.NOMEO = item.Contains("title") ? item["title"].ToString() : "";
-                        obj.NUMRUCEO = item.Contains("ticketnumber") ? item["ticketnumber"].ToString() : "";
-
-                        listaExcel.Add(obj);
-
-                    }
-
-                    String UrlFileExcel = "";
-
-                    string path = @"C:\Program Files\Microsoft Dynamics CRM\CRMWeb\Test\";
-                    path = export.Exportar_Excel(path, listaExcel);
-
-                    UrlFileExcel = "/test/" + path.Replace(@"C:\Program Files\Microsoft Dynamics CRM\CRMWeb\Test\", "");
-                    UrlFile.Set(executionContext, UrlFileExcel);
-
-                }
+              switch (LookupCaseType)
+              {
+                case ("05 - Reclamo OSIPTEL"):
+                  TipoCaso = "RQJ";
+                  break;
+                case ("06-Queja OSIPTEL"):
+                  TipoCaso = "RA";
+                  break;
+                default:
+                  TipoCaso = "";
+                  break;
+              }
             }
-            catch (Exception)
-            {
-                throw;
-            }
+
+            Trasu obj = new Trasu();
+            obj.NOMEO = columna_NOMEO.ToString() != null ? columna_NOMEO.ToString() : "";
+            obj.NUMRUCEO = columna_NUMRUCEO.ToString() != null ? columna_NUMRUCEO.ToString() : "";
+            obj.EMAILEO = columna_EMAILEO.ToString() != null ? columna_EMAILEO.ToString() : "";
+            obj.NUMCARTALTRASU = columna_NUMCARTALTRASU.ToString() != null ? columna_NUMCARTALTRASU.ToString() + TipoCaso + "/" : "";
+            obj.FECCARTALTRASU = FechaActual != null ? FechaActual : "";
+            obj.NUMFOLCARTALTRASU = "";
+            obj.CLARECURSO = TipoCaso != null ? TipoCaso : "";
+            obj.CODSERVCONTRATADO = "";
+            obj.NUMSERVABONADO = "";
+            obj.NUMSERVRECLAMO = "";
+            obj.CODSUBCRECLAMO = "";
+            obj.NUMRECIBORECLAMO = "";
+            obj.FECRECIBORECLAMO = "";
+            obj.FECVENCRECIBRECLAMO = "";
+
+            var start = item.GetAttributeValue<DateTime>("ust_serviceissuestartdate").ToString() !=null && item.GetAttributeValue<DateTime>("ust_serviceissuestartdate").ToString() != "1/1/0001 12:00:00 AM" ? item.GetAttributeValue<DateTime>("ust_serviceissuestartdate").ToString() : "";
+            var end = item.GetAttributeValue<DateTime>("ust_serviceissueenddate").ToString() != null && item.GetAttributeValue<DateTime>("ust_serviceissueenddate").ToString() != "1/1/0001 12:00:00 AM" ? item.GetAttributeValue<DateTime>("ust_serviceissueenddate").ToString() : "";
+
+            obj.DESPERIODRECLAMO = "Fecha Inicio Averia: " + start.ToString() + "-\n" + "Fecha Fin Averia: " + end.ToString();
+            obj.MONRECLAMO = "" ;
+            obj.CODMONEDARECLAMO = "";
+
+            //obj.NUMRECIBORECLAMO = "";
+            //obj.NUMRECIBORECLAMO = "";
+            //obj.NUMRECIBORECLAMO = "";
+            //obj.NUMRECIBORECLAMO = "";
+            //obj.NUMRECIBORECLAMO = "";
+
+
+
+
+
+
+            listaExcel.Add(obj);
+
+          }
+
+          String UrlFileExcel = "";
+
+          string path = @"C:\Program Files\Microsoft Dynamics CRM\CRMWeb\Test\";
+          path = export.Exportar_Excel(path, listaExcel);
+
+          UrlFileExcel = "/test/" + path.Replace(@"C:\Program Files\Microsoft Dynamics CRM\CRMWeb\Test\", "");
+          UrlFile.Set(executionContext, UrlFileExcel);
 
         }
-
-        [Output("UrlFile")]
-        public OutArgument<string> UrlFile { get; set; }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
 
     }
+
+
+
+    public string GetCrmConfiguration(IOrganizationService _service, string name)
+    {
+      string value = string.Empty;
+      string xml = "<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>" +
+                    "<entity name='etel_crmconfiguration'>" +
+                    "<attribute name='etel_crmconfigurationid'/>" +
+                    "<attribute name='etel_name'/>" +
+                    "<attribute name='etel_value'/>" +
+                    "<order descending='false' attribute='etel_value'/>" +
+                    "<filter type='and'> " +
+                    "<condition attribute='etel_name' value='" + name + "' operator='eq'/>" +
+                    "</filter>" +
+                    "</entity>" +
+                    "</fetch>";
+
+      EntityCollection configs = _service.RetrieveMultiple(new FetchExpression(xml));
+      Dictionary<string, string> configurations = new Dictionary<string, string>();
+
+      if (configs != null && configs.Entities != null)
+      {
+        var entity = configs.Entities.FirstOrDefault();
+        value = entity.Attributes["etel_value"].ToString();
+      }
+
+      return value;
+    }
+
+
+
+
+    [Output("UrlFile")]
+    public OutArgument<string> UrlFile { get; set; }
+
+  }
 
 
 }
